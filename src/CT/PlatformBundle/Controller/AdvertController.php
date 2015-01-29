0c24950fae6c4d855237bc8b6f74d3b792b2ed6b
Varlet Nicolas <nicolas_varlet@ens.univ-artois.fr>
2015-01-29 23:28:25 +0100

    Création de routes

--- src/CT/PlatformBundle/Controller/AdvertController.php
+++ src/CT/PlatformBundle/Controller/AdvertController.php
@@ -4,12 +4,22 @@
 
 namespace CT\PlatformBundle\Controller;
 
-// N'oubliez pas ce use :
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
+  public function viewSlugAction($slug, $year, $format){
+    return new Response(
+        "On pourrait afficher l'annonce correspondant au
+        slug '".$slug."', créée en ".$year." et au format ".$format."."
+    );
+  }
+
+  public function viewAction($id)
+  {
+    return new Response("Affichage de l'annonce d'id : ".$id);
+  }
   public function indexAction()
   {
     $content = $this->get('templating')->render('CTPlatformBundle:Advert:index.html.twig', array('nom' => 'Nico'));
